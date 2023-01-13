c132896055e63cf6a24c33725d63eab0ae8f09a7
softwaretestlab <softwaretestlab@hotmail.com>
2023-01-13 12:08:21 +1100

    array

---
+++ src/test/java/JavaLearning/AT07_staticmethod01.java
@@ -0,0 +1,13 @@
+package JavaLearning;
+
+import org.junit.Test;
+
+public class AT07_staticmethod01 {
+//static methods cant be run ridectly
+    // we have to call them in another class
+    @Test
+    public static void test11(){
+
+        System.out.println("This is static method");
+    }
+}
