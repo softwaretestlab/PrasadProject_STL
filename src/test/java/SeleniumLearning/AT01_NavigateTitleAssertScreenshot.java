4f875fdbca57273042a2b1ce1322a42a8bfb28ac
softwaretestlab <softwaretestlab@hotmail.com>
2023-01-27 10:12:06 +1100

    excell

--- src/test/java/SeleniumLearning/AT01_NavigateTitleAssertScreenshot.java
+++ src/test/java/SeleniumLearning/AT01_NavigateTitleAssertScreenshot.java
@@ -1,13 +1,12 @@
 package SeleniumLearning;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import BaseClass.OpenBrowser;
+import PropertiesFile.ReadPropertiesFile;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,20 +16,25 @@ public class AT01_NavigateTitleAssertScreenshot {
 
     @Test
     public void Navigation() throws IOException {
-        //Chromedriver setup
-        WebDriverManager.chromedriver().setup();
-        //this below line will open chrome browser
-        WebDriver driver = new ChromeDriver();
-        //maximize browser
-        driver.manage().window().maximize();
-        //Navigate to the website
-        driver.get("https://www.thegoodguys.com.au/");
+//        //Chromedriver setup
+//        WebDriverManager.chromedriver().setup();
+//        //this below line will open chrome browser
+//        WebDriver driver = new ChromeDriver();
+//        //maximize browser
+//        driver.manage().window().maximize();
+//        //Navigate to the website
+//        driver.get("https://www.thegoodguys.com.au/");
+    //---------------------    above code is replaced by below one line  -----------------------------------------
+     //   OpenBrowser.NavToWeb("https://www.thegoodguys.com.au/");
+        //---url reading form properties file
+        OpenBrowser.NavToWeb(ReadPropertiesFile.PropFile("HomePageUrl"));
+
         //implicit wait
         //if it takes more than 20sec , test case will fail and
         //we see pageloadtimeout error message
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        OpenBrowser.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         //get title of the page
-        String ActualTitle= driver.getTitle();
+        String ActualTitle= OpenBrowser.driver.getTitle();
         System.out.println("ActualTitle : "+ActualTitle);
         String ExpectedTitle="The Good Guys - Online Electrical & Home Appliances";
         //Assertion version-1
@@ -38,9 +42,9 @@ public class AT01_NavigateTitleAssertScreenshot {
         //Assertion version-2
         Assert.assertTrue(ActualTitle.contains("Electrical"));
         //screenshot
-        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File screenshotFile = ((TakesScreenshot)OpenBrowser.driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenshotFile, new File(System.getProperty("user.dir")+"\\src\\test\\java\\screenshots\\capture-1.jpeg"));
-
+        OpenBrowser.QuitDriver();
     }
 
 }
